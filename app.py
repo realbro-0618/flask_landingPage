52646923a2c034b67182217a01dcd3e3e0e95031
realbro-0618 <jinhyeong18@gmail.com>
2026-09-03 10:09:59 +0900

    commit test

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def default():
+	return render_template("index.html")
+
 @app.route("/main")
 def main():
 	return render_template("main.html")
